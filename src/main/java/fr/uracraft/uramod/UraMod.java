04bc9f174f34f348d78c79d68cd1b1f6d4a262e0
Birdo3D <52548243+Birdo3D@users.noreply.github.com>
2021-05-02 22:25:53 +0200

    Customs Debug Layout and lang files modification.

--- src/main/java/fr/uracraft/uramod/UraMod.java
+++ src/main/java/fr/uracraft/uramod/UraMod.java
@@ -1,5 +1,6 @@
 package fr.uracraft.uramod;
 
+import fr.uracraft.uramod.events.DebugLayout;
 import fr.uracraft.uramod.events.EventCustomMainMenu;
 import fr.uracraft.uramod.proxy.CommonProxy;
 import net.minecraft.block.Block;
@@ -15,46 +16,42 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = UraMod.MODID, name = UraMod.NAME, version = UraMod.VERSION)
 public class UraMod {
 
-        public static final String MODID = "uramod";
-        public static final String NAME = "UraMod";
-        public static final String VERSION = "0.1";
+    public static final String MODID = "uramod";
+    public static final String NAME = "UraMod";
+    public static final String VERSION = "0.1";
 
-        private static Logger logger;
+    private static Logger logger;
 
-        @Mod.Instance(UraMod.MODID)
-        public static UraMod instance;
+    @Mod.Instance(UraMod.MODID)
+    public static UraMod instance;
 
-        @SidedProxy(clientSide="fr.uracraft.uramod.proxy.ClientProxy", serverSide="fr.uracraft.uramod.proxy.CommonProxy")
-        public static CommonProxy proxy;
+    @SidedProxy(clientSide = "fr.uracraft.uramod.proxy.ClientProxy", serverSide = "fr.uracraft.uramod.proxy.CommonProxy")
+    public static CommonProxy proxy;
 
-        public static final String ip = "localhost";
-        public static final int port = 25565;
-        public final boolean singleplayer = true;
+    public static final String ip = "localhost";
+    public static final int port = 25565;
+    public final boolean singleplayer = true;
 
-        public UraMod()
-        {
-                MinecraftForge.EVENT_BUS.register(new RegisteringHandler());
-        }
+    public UraMod() {
+        MinecraftForge.EVENT_BUS.register(new RegisteringHandler());
+    }
 
-        @Mod.EventHandler
-        public void preInit(FMLPreInitializationEvent event)
-        {
-            logger = event.getModLog();
-            MinecraftForge.EVENT_BUS.register(new EventCustomMainMenu());
-        }
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+        MinecraftForge.EVENT_BUS.register(new EventCustomMainMenu());
+        MinecraftForge.EVENT_BUS.register(new DebugLayout());
+    }
 
-        @Mod.EventHandler
-        public void init(FMLInitializationEvent event)
-        {
-                proxy.registerRender();
-                RegisteringHandler.registerEntities();
-                UraFurnaceRecipes.registerFurnaceRecipes(event);
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        proxy.registerRender();
+        RegisteringHandler.registerEntities();
+        UraFurnaceRecipes.registerFurnaceRecipes(event);
+    }
 
-        }
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
 
-        @Mod.EventHandler
-        public void postInit(FMLPostInitializationEvent event)
-        {
-
-        }
+    }
 }
